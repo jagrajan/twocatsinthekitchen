21d9a7c83c5d7fdd1aa255d86337b76624b4598a
Jagrajan Bhullar <jag@jagrajan.com>
2020-05-12 19:10:01 -0700

    add RecipeInfo component

--- client/src/containers/Home/RecentRecipes.tsx
+++ client/src/containers/Home/RecentRecipes.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
+import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
+import RecipeInfo from './RecipeInfo';
 import { RecipeShortInformation } from 'store/recipe/types';
+import { IMAGE_SERVER } from 'config';
 
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'grid',
     gridTemplateColumns: '1fr',
+    gridGap: '1.5rem',
     [theme.breakpoints.up('md')]: {
-      gridTemplateColumns: '1fr 1fr'
-    }
-  }
+      gridTemplateColumns: '1fr 1fr 1fr',
+    },
+  },
 }));
 
 type Props = {
@@ -20,10 +24,14 @@ const RecentRecipes: FC<Props> = ({ recentRecipes }) => {
   const classes = useStyles();
   return (
     <div className={classes.container}>
-      <p>This is a something</p>
-    {recentRecipes.map(recipe => <div>
-    {recipe.info.name}
-    </div>)}
+      {recentRecipes.map(recipe =>
+        <RecipeInfo
+          name={recipe.info.name}
+          imageFile={`${IMAGE_SERVER}/${recipe.info.image_file}`}
+          updateDate={moment(recipe.info.last_update)
+            .format('MMMM Do YYYY, h:mm:ss a')}
+        />
+      )}
     </div>
   );
 };
---
+++ client/src/containers/Home/RecipeInfo.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import UndecoratedLink from 'components/UndecoratedLink';
+
+const useStyles = makeStyles(theme => ({
+  image: {
+    opacity: 0,
+    width: '100%',
+  },
+}));
+
+type Props = {
+  name: string;
+  imageFile: string;
+  updateDate: string;
+};
+
+const RecipeInfo: FC<Props> = ({ imageFile, name, updateDate }) => {
+  const classes = useStyles();
+  return <UndecoratedLink to="/test">
+    <Card>
+      <CardHeader
+        title={name}
+        subheader={updateDate}
+      />
+      <CardMedia
+        image={imageFile}
+        title={name}
+      >
+        <img
+          className={classes.image}
+          src={imageFile}
+          alt={name}
+        />
+      </CardMedia>
+    </Card>
+  </UndecoratedLink>;
+};
+
+export default RecipeInfo;
