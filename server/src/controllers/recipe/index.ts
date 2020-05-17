8471d591fbbbb0bc2751c2773f3bdfb3181f2bb9
Jagrajan Bhullar <jag@jagrajan.com>
2020-05-17 23:08:49 +0000

    fix #5: recipe details route and controller

--- server/src/controllers/recipe/index.ts
+++ server/src/controllers/recipe/index.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import client from 'db';
+import Recipe from 'models/cookbook/Recipe';
 import RecipeFetcher from 'models/cookbook/RecipeFetcher';
 import RecipeVersionFetcher from 'models/cookbook/RecipeVersionFetcher';
 
@@ -24,6 +26,74 @@ export const getRecent = async (req: Request, res: Response): Promise<void> => {
     }
   } else {
     response.error = 'No recipes found';
+  }
+  res.json(response);
+};
+
+export const getDetails = async(req: Request, res: Response): Promise<void> => {
+  let response: any = {};
+  let input: string = req.params.id;
+  let field: string = 'id';
+
+  if (input && isNaN(input as any)) {
+    field  = 'slug';
+  }
+  const recipeFetcher = new RecipeFetcher();
+  const recipeData  = await recipeFetcher.getDataByFieldsEqual({ [field]: input });
+  if (recipeData != null) {
+    const recipeVersionFetcher = new RecipeVersionFetcher();
+    const version = await recipeVersionFetcher
+      .getReleasedVersion(new Recipe(recipeData[0]));
+    if (version != null) {
+
+      // Get ingredients for this recipe
+      const ingRes = await client.query(`
+        SELECT
+          mi.min_amount,
+          mi.max_amount,
+          mi.position,
+          u.name AS unit_name,
+          u.plural AS unit_plural,
+          i.name AS ingredient_name,
+          i.plural AS ingredient_plural
+        FROM cookbook.measured_ingredient mi
+        INNER JOIN cookbook.ingredient i
+          ON i.id = mi.ingredient_id
+        INNER JOIN cookbook.unit u
+          ON u.id = mi.unit_id
+        WHERE mi.recipe_version_id = $1
+      `, [version.dataValues.id]);
+
+      // Get notes for this recipe
+      const notesRes = await client.query(`
+        SELECT
+          rn.text,
+          rn.position
+        FROM cookbook.recipe_note rn
+        WHERE rn.global = true AND rn.recipe_version_id = $1
+      `, [version.dataValues.id]);
+
+      // Get steps for thsi recipe
+      const stepsRes = await client.query(`
+        SELECT
+          rs.description,
+          rs.position
+        FROM cookbook.recipe_step rs
+        WHERE rs.recipe_version_id = $1
+      `, [version.dataValues.id]);
+
+      response.recipe = {
+        metadata: recipeData[0],
+        info: version?.dataValues,
+        ingredients: ingRes.rows,
+        notes: notesRes.rows,
+        steps: stepsRes.rows,
+      };
+    }
+  }
+
+  if (!response.recipe) {
+    response.error = 'No recipe found';
   }
   res.json(response);
 };
--- server/src/routes/api/RecipeRouter.ts
+++ server/src/routes/api/RecipeRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import AsyncHandler from 'express-async-handler';
 import {
+  getDetails,
   getRecent
 } from 'controllers/recipe';
 
 const router: Router = Router();
 
 router.get('/recent', AsyncHandler(getRecent));
+router.get('/:id', AsyncHandler(getDetails));
 
 export default router;
