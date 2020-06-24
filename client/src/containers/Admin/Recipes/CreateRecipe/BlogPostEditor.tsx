b3d34f04e75119f934b64ea0c8aab6ba73bd8c40
Jagrajan Bhullar <jag@jagrajan.com>
2020-06-24 00:16:17 -0700

    [BLOG-2] fix slash issues

--- client/src/containers/Admin/Recipes/CreateRecipe/BlogPostEditor.tsx
+++ client/src/containers/Admin/Recipes/CreateRecipe/BlogPostEditor.tsx
@@ -1,34 +1,24 @@
-import React, { FC, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@twocats/store';
-import { setIntroduction } from 'store/recipeEditor/actions';
-import MarkdownEditor, {  } from 'rich-markdown-editor';
-import theme from './markdownConfig';
-import Container from '@material-ui/core/Container';
-import { uploadBlogImage } from 'services/api/api-recipe-editor';
-import { makeStyles } from '@material-ui/core/styles';
-import { IMAGE_SERVER } from 'config';
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import { RootState } from "@twocats/store";
+import { IMAGE_SERVER } from "config";
+import React, { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import MarkdownEditor from "rich-markdown-editor";
+import { uploadBlogImage } from "services/api/api-recipe-editor";
+import { setIntroduction } from "store/recipeEditor/actions";
+import theme from "./markdownConfig";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   container: {
-    margin: '2rem auto',
+    margin: "2rem auto",
   },
 }));
 
 const content = `
-## Heading 2
+## A heading
 
-![](http://localhost:4000/image/196fcf0d-6524-4976-8a01-8d85132278ab.jpg)
-
-This is a recipe introduction. I talk a lot as I talk about food with you. What do you think about my food!
-
-This is a recipe introduction. I talk a lot as I talk about food with you. What do you think about my food!
-
-This is a recipe introduction. I talk a lot as I talk about food with you. What do you think about my food!
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis quam risus, ac fermentum metus [lacinia et](https://wikipedia.org). Ut volutpat sapien id mattis accumsan. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque vitae tempor lorem, non.
-
-![](http://localhost:4000/image/f116c862-85b2-4558-b979-a0e8ddefb886.jpg)
+Here's a little something to get your started!
 
 `;
 
@@ -41,10 +31,9 @@ const BlogPostEditor: FC = () => {
   const loading = useSelector<RootState, boolean>((state: RootState) => {
     return state.recipeEditor.loading;
   });
-  console.log(introduction);
 
   useEffect(() => {
-    dispatch(setIntroduction(content))
+    dispatch(setIntroduction(content));
   }, [dispatch]);
 
   async function uploadImageHandler(file: File): Promise<string> {
@@ -54,14 +43,18 @@ const BlogPostEditor: FC = () => {
 
   return (
     <Container className={classes.container}>
-      {!loading && <MarkdownEditor
-        defaultValue={introduction}
-        onChange={(value) => dispatch(setIntroduction(value()))}
-        theme={theme}
-        uploadImage={uploadImageHandler}
-      />}
+      {!loading && (
+        <MarkdownEditor
+          defaultValue={introduction}
+          onChange={(value) => {
+            dispatch(setIntroduction(value().replace(/^\\/gm, '')));
+          }}
+          theme={theme}
+          uploadImage={uploadImageHandler}
+        />
+      )}
     </Container>
   );
-}
+};
 
 export default BlogPostEditor;
